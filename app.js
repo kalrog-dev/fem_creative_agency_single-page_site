4d66a8f168a1230a9965a943ae4fc3215af120e9
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2023-10-09 20:57:43 +0200

    Add JS for slider and navigation, define grid areas, add list item counter

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
-// Open and close mobile navigation
+/* ==================================
+  Open and close mobile navigation
+===================================== */
 const menuBtn = document.querySelector(".menu-btn");
+const menuIcon = document.querySelector(".menu-icon");
 const nav = document.querySelector(".main-nav");
 
+// TODO: Close menu when a link is clicked
 menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("close-nav");
+  // Navigation is closed
+  if (nav.classList.contains("close-nav")) {
+    // Open navigation
+    nav.classList.remove("close-nav");
+    menuIcon.setAttribute("src", "./assets/mobile/icon-cross.svg");
+  } else {
+    // Close navigation
+    nav.classList.add("close-nav");
+    menuIcon.setAttribute("src", "./assets/mobile/icon-hamburger.svg");
+  }
 });
 
-// TODO: Toggle between hamburger and close icon
+/* ==================================
+  Slider
+===================================== */
+let currentSlide = 0;
+const btnPrev = document.querySelector(".grid__btn-prev");
+const btnNext = document.querySelector(".grid__btn-next");
+const slideImageContainer = document.querySelector(".grid__slider-image-container");
+
+// Clamp current slide
+function clamp(num, min, max) {
+  if (num < min) {
+    num = min;
+  } else if (num > max) {
+    num = max;
+  }
+  return num;
+}
+
+// Arrow button listeners
+btnPrev.addEventListener("click", () => {
+  currentSlide--;
+  currentSlide = clamp(currentSlide, 0, 2);
+  slideImageContainer.style.transform = `translateX(${currentSlide * -100}vw)`;
+});
+
+btnNext.addEventListener("click", () => {
+  currentSlide++;
+  currentSlide = clamp(currentSlide, 0, 2);
+  slideImageContainer.style.transform = `translateX(${currentSlide * -100}vw)`;
+});
